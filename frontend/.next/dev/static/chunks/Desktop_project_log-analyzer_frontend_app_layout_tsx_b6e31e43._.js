f58c6c11b8e6116f90a1fbd213462af7722cd660
Ravipati Madhulika <ravipatimadhulika205@gmail.com>
2026-01-01 16:07:04 +0530

    Delete frontend directory

--- frontend/.next/dev/static/chunks/Desktop_project_log-analyzer_frontend_app_layout_tsx_b6e31e43._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/Desktop_project_log-analyzer_frontend_app_globals_2c80cae3.css",
-  "static/chunks/03845_next_dist_0801601a._.js"
-],
-    source: "dynamic"
-});
--- frontend/.next/dev/static/chunks/Desktop_project_log-analyzer_frontend_app_upload_page_tsx_d13c4d72._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/03845_c11fa57d._.js",
-  "static/chunks/Desktop_project_log-analyzer_frontend_d215b882._.js"
-],
-    source: "dynamic"
-});
